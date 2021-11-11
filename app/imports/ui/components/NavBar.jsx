6dc9aaee58fa80f532de4fd5ebc7b2941533bbb6
hwangkyh <70243047+hwangkyh@users.noreply.github.com>
2021-11-10 21:34:33 -1000

    hacc-tech-review

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,24 +3,30 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown, Header } from 'semantic-ui-react';
+import { Menu, Image, Header, Dropdown } from 'semantic-ui-react';
 import { Roles } from 'meteor/alanning:roles';
+
+const kahukai = '/images/Kahukai_transparent.png';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
   render() {
-    const menuStyle = { marginBottom: '10px' };
     return (
-      <Menu style={menuStyle} attached="top" borderless inverted>
+      <Menu attached="top" borderless id="nav" className="ui stackable menu">
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
-          <Header inverted as='h1'>meteor-application-template</Header>
+          <Image src={kahukai} size='small' alt='Kahukai app logo'/>
         </Menu.Item>
-        {this.props.currentUser ? (
-          [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
-        ) : ''}
+        <Menu.Item as={NavLink} activeClassName="" exact to="/distress">
+          <Header as="h4">Distress Report</Header>
+        </Menu.Item>
+        <Menu.Item as={NavLink} activeClassName="" exact to="/sighting">
+          <Header as="h4">Sighting Report</Header>
+        </Menu.Item>
+        <Menu.Item as={NavLink} activeClassName="" exact to="/infodistress">
+          <Header as="h4">Info</Header>
+        </Menu.Item>
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-          <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/adminlist" key='adminlist'><Header as="h4">Reports List</Header></Menu.Item>
         ) : ''}
         <Menu.Item position="right">
           {this.props.currentUser === '' ? (
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Button, Grid, Image, Container } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
+      <Container fluid>
+        <Grid id='landing-page' className='center aligned'>
+          <Grid.Row verticalAlign='middle'>
+            <Grid.Column width={14}>
+              <h3 className="intro">Welcome to Kahukai: the HMAR Reporting app</h3>
+              <p>Select an option from below:</p>
+              <Button as={NavLink} className="distressButton" exact to="/distress" color='red'>Animal in distress</Button>
+              <Image src='https://www.mauibath.com/wp-content/uploads/2019/10/596998840197290025-300x300.png' size='small' centered className="logo"/>
+              <Button as={NavLink} className="sightingButton" exact to="/sighting" color='blue'>Animal sighting</Button>
+              <p className="clickHere">Don&apos;t know what an animal in distress looks like?</p>
+              <Button as={NavLink} className="infoButton" exact to="/infodistress">Click here</Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
