002fe5d616edb159381caf42d590154ccf3cf8b4
hwangkyh <70243047+hwangkyh@users.noreply.github.com>
2021-12-15 13:33:49 -1000

    Update SealTable.jsx

--- app/imports/ui/pages/SealTable.jsx
+++ app/imports/ui/pages/SealTable.jsx
@@ -17,6 +17,8 @@ const columns = [
   { name: 'Markers', selector: row => row.markers, sortable: true },
   { name: 'Number of People', selector: row => row.numPeople, sortable: true },
   { name: 'Image', selector: row => row.image },
+  { name: 'Edit', selector: row => <Link to={`/edit/${row._id}`}>Edit</Link> },
+  { name: 'Delete', selector: row => <Link to={`/delete/${row._id}`}>Delete</Link> },
 ];
 
 export const SealTable = (props) => {
