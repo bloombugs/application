803d484a0a9425025a340e47efce73c1fd530031
hwangkyh <70243047+hwangkyh@users.noreply.github.com>
2021-12-15 15:31:06 -1000

    Update Other.jsx

--- app/imports/ui/pages/Other.jsx
+++ app/imports/ui/pages/Other.jsx
@@ -11,13 +11,13 @@ class Other extends React.Component {
         <Grid.Row verticalAlign='middle'>
           <Grid.Column width={14}>
             <div className="ui hidden divider"></div>
-            <Header as="h2" style={{ color: '#f6dcae' }}>Unfortunately HMAR only responds to seals, turtles and birds.</Header>
-            <Header as="h3">Below are numbers of other organzations that respond to other wildlife </Header>
+            <Header as="h2" style={{ color: '#f6dcae' }}>Unfortunately HMAR only responds to Hawaiian Monk Seals, Sea Turtles and Seabirds.</Header>
+            <Header as="h3">Below are numbers of other organzations that respond to other wildlife.</Header><br/>
             <Image src={hwc} size="tiny" centered alt='Hawaii Wildlife Center logo'/>
             <Header as="h3" >Hawaii Wildlife Center:<br/>Bird and Bat Help Line<br/>(808) 884-5000</Header>
             <Image src={dlnr} size="tiny" centered alt='State of Hawaii Department of Land and Natural Resources logo'/>
-            <Header as="h3"> State of Hawaii:<br/>Department of Land and Natural Resources<br/>Marine Animal Response Hotline<br/>1-888-256-9840</Header>
-            <Header as="h3"> Honolulu Police Department&apos;s<br/>Non-emergency line: (808) 529-3111</Header>
+            <Header as="h3"> State of Hawaii:<br/>Department of Land and Natural Resources<br/>Marine Animal Response Hotline<br/>(888) 256-9840</Header>
+            <Header as="h3"> Honolulu Police Department:<br/>Non-Emergency Line<br/>(808) 529-3111</Header>
             <div className="ui hidden divider"></div>
           </Grid.Column>
         </Grid.Row>
