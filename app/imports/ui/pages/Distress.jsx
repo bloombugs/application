f89b1388610ac368b457f92fb49d3c9f91043129
hwangkyh <70243047+hwangkyh@users.noreply.github.com>
2021-12-15 16:06:30 -1000

    Update Distress.jsx

--- app/imports/ui/pages/Distress.jsx
+++ app/imports/ui/pages/Distress.jsx
@@ -15,8 +15,10 @@ class Distress extends React.Component {
           <Grid.Column width={14}>
             <div className="ui hidden divider"></div>
             <Header as="h3" style={{ color: 'white' }}>If the animal is in distress, it is preferable to call HMAR
-              so they can attend to the animal as soon as possible <a className="white-link" href="tel:[888-256-9840]">(888) 256-9840</a>. However, if you cannot call, please fill out the form</Header>
-            <Button animated size='big' color='red'>
+              so they can attend to the animal as soon as possible <a className="white-link" href="tel:[888-256-9840]">(888) 256-9840</a>. However, if you cannot call, please fill out the online reporting form.</Header>
+            <hr/>
+            <Button animated size='big' color='red' icon labelPosition='left'>
+            <Icon name='phone volume'/>
               <Button.Content visible>Click for Phone Call</Button.Content>
               <a href="tel:[888-256-9840]">
                 <Button.Content hidden>
@@ -24,11 +26,15 @@ class Distress extends React.Component {
                 </Button.Content>
               </a>
             </Button>
-            <hr/>
             <Button size='big' color='green' icon labelPosition='right'>
               <Icon name='file text'/>
               <Link to="/distressform" id='distressFormPage' style={{ color: 'white' }}>
                 Click for Online Form
+              </Link>
+            </Button>
+            <Button size='medium' color='grey'>
+              <Link to="/infodistress" style={{ color: 'white' }}>
+                How to tell if an animal is in distress
               </Link>
             </Button>
             <div className="ui hidden divider" style={{ color: 'white', paddingBottom: '10%' }}/>
