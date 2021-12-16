ed888de9fdac178c9ebf6e57a67cd27eb8d05d5c
hwangkyh <70243047+hwangkyh@users.noreply.github.com>
2021-12-15 15:51:29 -1000

    Update Other.jsx

--- app/imports/ui/pages/Other.jsx
+++ app/imports/ui/pages/Other.jsx
@@ -15,11 +15,16 @@ class Other extends React.Component {
             <Header as="h2" style={{ color: '#f6dcae' }}>Unfortunately HMAR only responds to Hawaiian Monk Seals, Sea Turtles and Seabirds.</Header>
             <Header as="h3">Below are numbers of other organzations that respond to other wildlife.</Header><br/>
             <Image src={hwc} size="tiny" centered alt='Hawaii Wildlife Center logo'/>
-            <Header as="h3" >Hawaii Wildlife Center:<br/>Bird and Bat Help Line<br/>(808) 884-5000</Header>
+            <Header as="h3">Hawaii Wildlife Center:<br/>Rescue and Response<br/></Header><p> for all native species</p>
+              <Header as="h3"><a className="black-link" href="tel:[808-884-5000]">(808) 884-5000</a></Header>
             <Image src={dlnr} size="tiny" centered alt='State of Hawaii Department of Land and Natural Resources logo'/>
-            <Header as="h3"> State of Hawaii:<br/>Department of Land and Natural Resources<br/>Marine Animal Response Hotline<br/>(888) 256-9840</Header>
+            <Header as="h3"> State of Hawaii:<br/>Department of Land and Natural Resources<br/>Oahu Division of Forestry and Wildlife (DOFAW)<br/></Header><p> for all native species</p>
+              <Header as="h3"><a className="black-link" href="tel:[808-973-9786]">(808) 973-9786</a>
+              <a className="black-link" href="tel:[808-295-5896]">(808) 295-5896</a>
+              <a className="black-link" href="tel:[808-226-6050]">(808) 226-6050</a></Header>
             <Image src={hpd} size="tiny" centered alt='Honolulu Police Department logo'/>
-            <Header as="h3"> Honolulu Police Department:<br/>Non-Emergency Line<br/>(808) 529-3111</Header>
+            <Header as="h3"> Honolulu Police Department:<br/>Non-Emergency Line<br/>
+              <a className="black-link" href="tel:[808-529-3111]">(808) 529-3111</a></Header>
             <div className="ui hidden divider"></div>
           </Grid.Column>
         </Grid.Row>
